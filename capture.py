059e409057381c768e0be92fbe6b4af87f64463e
juanasalamanco <juanasalamanco@gmail.com>
2023-06-30 11:18:41 -0300

    update

---
+++ capture.py
@@ -0,0 +1,16 @@
+import cv2
+print(cv2.__version__)
+
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    ret, frame = vid.read()
+  
+    cv2.imshow('frame', frame)
+      
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+vid.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,17 +1,114 @@
 import IPython 
-#from IPython.display import display, Javascript, Image
-
 import cv2
-#import cv2
-
-#from google.colab.output import eval_js
-#from google.colab.patches import cv2_imshow
-#from base64 import b64decode, b64encode
-
-#import numpy as np
-#import PIL
-#import io
-#import html
-#import time
-#import matplotlib.pyplot as plt
+from base64 import b64decode, b64encode
+import numpy as np
+import PIL
+import io
+import html
+import time
+import matplotlib.pyplot as plt
 #%matplotlib inline
+#!git clone https://github.com/AlexeyAB/darknet
+#%cd darknet
+
+
+###
+# !sed -i 's/OPENCV=0/OPENCV=1/' Makefile
+# !sed -i 's/GPU=0/GPU=1/' Makefile
+# !sed -i 's/CUDNN=0/CUDNN=1/' Makefile
+# !sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
+# !sed -i 's/LIBSO=0/LIBSO=1/' Makefile
+
+# !make
+
+# !wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1V3vsIaxAlGWvK4Aar9bAiK5U0QFttKwq" -O yolov4-csp.weights && rm -rf /tmp/cookies.txt
+
+
+
+#DARKNET PARA PYTHON----------------
+from darknet import *
+network, class_names, class_colors = load_network("cfg/yolov4-csp.cfg", "cfg/coco.data", "yolov4-csp.weights")
+width = network_width(network)
+height = network_height(network)
+
+def darknet_helper(img, width, height):
+  darknet_image = make_image(width, height, 3)
+  img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img_resized = cv2.resize(img_rgb, (width, height), interpolation=cv2.INTER_LINEAR)
+
+  img_height, img_width, _ = img.shape
+  width_ratio = img_width/width
+  height_ratio = img_height/height
+
+  copy_image_from_bytes(darknet_image, img_resized.tobytes())
+  detections = detect_image(network, class_names, darknet_image)
+  free_image(darknet_image)
+  return detections, width_ratio, height_ratio
+
+#FUNCIONES PARA CONVERTIR ENTRE DIFERENTES TIPOS DE IMAGENES------------------
+def js_to_image(js_reply):
+  """
+  Params:
+          js_reply: JavaScript object containing image from webcam
+  Returns:
+          img: OpenCV BGR image
+  """
+  image_bytes = b64decode(js_reply.split(',')[1])
+  jpg_as_np = np.frombuffer(image_bytes, dtype=np.uint8)
+  img = cv2.imdecode(jpg_as_np, flags=1)
+
+  return img
+
+def bbox_to_bytes(bbox_array):
+  """
+  Params:
+          bbox_array: Numpy array (pixels) containing rectangle to overlay on video stream.
+  Returns:
+        bytes: Base64 image byte string
+  """
+  bbox_PIL = PIL.Image.fromarray(bbox_array, 'RGBA')
+  iobuf = io.BytesIO()
+  bbox_PIL.save(iobuf, format='png')
+  bbox_bytes = 'data:image/png;base64,{}'.format((str(b64encode(iobuf.getvalue()), 'utf-8')))
+
+  return bbox_bytes
+
+#FUNCION PARA EL VIDEOSTREAM-------------------------------------------------------
+
+
+#CORRER POR WEBCAM
+video_stream()
+label_html = 'Capturing...'
+bbox = ''
+count = 0 
+while True:
+    js_reply = video_frame(label_html, bbox)
+    if not js_reply:
+        break
+
+    frame = js_to_image(js_reply["img"])
+
+    bbox_array = np.zeros([480,640,4], dtype=np.uint8)
+
+    detections, width_ratio, height_ratio = darknet_helper(frame, width, height)
+
+    for label, confidence, bbox in detections:
+      left, top, right, bottom = bbox2points(bbox)
+      left, top, right, bottom = int(left * width_ratio), int(top * height_ratio), int(right * width_ratio), int(bottom * height_ratio)
+      bbox_array = cv2.rectangle(bbox_array, (left, top), (right, bottom), class_colors[label], 2)
+      bbox_array = cv2.putText(bbox_array, "{} [{:.2f}]".format(label, float(confidence)),
+                        (left, top - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        class_colors[label], 2)
+
+    bbox_array[:,:,3] = (bbox_array.max(axis = 2) > 0 ).astype(int) * 255
+    bbox_bytes = bbox_to_bytes(bbox_array)
+    bbox = bbox_bytes
+
+    #PARA CONTAR LAS DETECCIONES
+    print(len(detections))
+    if len(detections) <= 50:
+      print("green")
+    elif len(detections) >= 50 and len(detections) <= 100:
+      print("yellow")
+    elif len(detections) >= 100 and len(detections) <= 169:
+      print("red")
